4f511a84cb4547d00b4ae1497f5328a4052e1097
Fortiny97 <jerusionfortiny@gmail.com>
2026-01-29 17:52:11 +0300

    Fix Vite base and add latest files

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // 👈 VERY IMPORTANT
+  base: "/", // Important for Vercel to find CSS/JS files
 });
